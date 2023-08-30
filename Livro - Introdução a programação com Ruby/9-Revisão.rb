42016f2fbaaedff6ebd6c390eb8c63e8f9d09ef1
Lia Farias <lia.farias@rdstation.com>
2023-08-29 21:06:36 -0300

    Revisão

---
+++ Livro - Introdução a programação com Ruby/9-Revisão.rb
@@ -0,0 +1,65 @@
+#1.Use o método each em um Array para iterar [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] e imprimir cada valor.
+
+puts ' '
+
+#2.O mesmo que acima, mas imprime apenas valores maiores que 5.
+
+puts ' '
+
+#3.Agora, usando o mesmo array do nº 2, use o método select para extrair todos os números ímpares para um novo array.
+
+puts ' '
+
+#4.Anexe 11 ao final da matriz (array) original. Anexe 0 ao início.
+
+puts ' '
+
+#5. Livre-se do 11. E acrescente um 3.
+
+puts ' '
+
+#6.Livre-se de duplicatas sem remover especificamente nenhum valor.
+
+puts ' '
+
+#7.Qual é a principal diferença entre um Array e um Hash?
+
+puts ' '
+
+#8.Crie um Hash, com um par chave-valor, usando ambos os estilos de sintaxe Ruby.
+
+puts ' '
+
+#9.Suponha que você tenha um hashh = {a:1, b:2, c:3, d:4}
+
+puts ' '
+
+#10.Os valores hash podem ser matrizes? Você pode ter uma variedade de hashes? (dar exemplos)
+
+puts ' '
+
+#11.Dadas as estruturas de dados a seguir, escreva um programa que copie as informações do array para o hash vazio que se aplica à pessoa correta.
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+# Expected output:
+#  {
+#    "Joe Smith"=>{:email=>"joe@email.com", :address=>"123 Main st.", :phone=>"555-123-4567"},
+#    "Sally Johnson"=>{:email=>"sally@email.com", :address=>"404 Not Found Dr.",  :phone=>"123-234-3454"}
+#  }
+
+puts ' '
+
+#12.Usando o hash criado no exercício anterior, demonstre como você acessaria o e-mail de Joe e o número de telefone de Sally.
+
+puts ' '
+
+#13.delete_ifUse o método Array e o método String do Ruby start_with?para excluir todas as strings que começam com "s" no array a seguir.
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+#Em seguida, recrie o arre livre-se de todas as strings que começam com "s" ou com "w".
+
+puts ' '
+
+#14.Pegue a seguinte matriz:
